58fdac05a2b93a84e07954bbedab8f01cf68e975
ankit sadangi <ankit.sadangi@thoughtfocus.com>
2019-11-04 15:32:46 +0530

    new

--- Check_Columns.py
+++ Check_Columns.py
@@ -3,13 +3,18 @@ def checkColumn(fle, fleName):
     from pandas import ExcelWriter
     from pandas import ExcelFile
     import numpy as np
+    import json
 
     checkColumnFile = 'https://s3.us-east.cloud-object-storage.appdomain.cloud/sharad-saurav-bucket/checkColumn.xlsx'
     df=pd.read_excel(checkColumnFile)
     columnNames =  df.loc[df.File == fleName,'Columns'].tolist()
- 
+    columnNames =  "[" + columnNames[0] +"]"
+    columnNames = json.loads(columnNames)
+    print('columnNames----------',columnNames)
+    # print('columnNames-----',columnNames)
     df = pd.read_excel(fle)
+    print('df----',df)
     for col in columnNames:
+        print('col---------',col)
         if col not in df.columns:
             raise KeyError("column " + col + " not present in " + fleName)
-            
