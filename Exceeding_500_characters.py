1ff0cc82a73f25f2c9da6ea71a81d4b47f0560db
ankit sadangi <ankit.sadangi@thoughtfocus.com>
2019-11-18 12:14:23 +0530

    new

--- Exceeding_500_characters.py
+++ Exceeding_500_characters.py
@@ -37,6 +37,9 @@ def exceeding_500_characters(fle, fleName, target):
 						print('The row '+str(index)+' in the file '+fleName+' has content greater than 500 characters in the '+column_name+' column')
 						data.append(entry)
 					
-		df1 = pd.DataFrame(data, columns = ['ROW_NO', 'FILE_NAME', 'COMMENTS'])
-		with ExcelWriter(target,engine='openpyxl',mode='a') as writer:
-			df1.to_excel(writer,sheet_name=rule,index=False)
+		if(ExcelFile(target).sheet_names[0] == 'Sheet1'):
+			with ExcelWriter(target, engine='openpyxl', mode='w') as writer:
+				df1.to_excel(writer,sheet_name=rule,index=False)
+		else:
+			with ExcelWriter(target, engine='openpyxl', mode='a') as writer:
+				df1.to_excel(writer,sheet_name=rule,index=False)
